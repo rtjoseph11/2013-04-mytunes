c2546711962f72980502ac43855bf01b6e9dd655
Hack Reactor Students <github@hackreactor.com>
2013-05-11 12:22:04 -0700

    SongQueue up to dequeue, working on dequeue

--- src/SongQueue.js
+++ src/SongQueue.js
@@ -1,6 +1,29 @@
 var SongQueue = Songs.extend({
+  model:Song,
 
+  //check back if we're doing proper event listening in the right place
   initialize: function(){
+
+    this.on('add',function() {
+      if (this.length===1) {
+        this.playFirst();
+      }
+    });
+
+    this.on('ended', function() {
+      this.shift();
+      if (this.length>0) {
+        this.playFirst();
+      }
+    });
+
+    this.on('dequeue', function() {
+
+    });
+  },
+
+  playFirst: function() {
+    this.at(0).play();
   }
 
 });
