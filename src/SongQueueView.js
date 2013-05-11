adffa2a56c688dda0dbf64a779a6c84deb0d5878
Hack Reactor Students <github@hackreactor.com>
2013-05-11 13:29:25 -0700

    SongQueueView render

--- src/SongQueueView.js
+++ src/SongQueueView.js
@@ -1,10 +1,23 @@
 var SongQueueView = Backbone.View.extend({
-
   initialize: function() {
+    // var self = t
+    this.collection.on('add',function() {
+      this.render();
+    },this);
+    this.collection.on('remove',function() {
+      this.render();
+    },this);
   },
 
   render: function() {
-    return this.$el;
-  }
+    // to preserve event handlers on child nodes, we must call .detach() on them before overwriting with .html()
+    // see http://api.jquery.com/detach/
+    this.$el.children().detach();
 
+    return this.$el.html('<th>Song Queue</th>').append(
+      this.collection.map(function(song){
+        return new SongQueueEntryView({model: song}).render();
+      })
+    );
+  }
 });
